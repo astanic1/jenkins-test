87bbb2772fcc41618df9303b2a83ae4093510add
Adam Stanic <adam.stanic@symphony.is>
2018-06-05 12:01:29 +0200

    brisanje

--- scheduling_services/dao/src/main/java/factory/AbstractFactory.java
+++
@@ -1,8 +0,0 @@
-package factory;
-
-import com.mysql.jdbc.NotImplemented;
-import sms.SmsDispatcher;
-
-public abstract class AbstractFactory {
-    public abstract SmsDispatcher getSmsDispatcher(String countryName) throws  NotImplemented;
-}
